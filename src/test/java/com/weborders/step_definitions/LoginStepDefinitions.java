1b8cdad98fe1ab1364693657c807407c278802a4
aliasici <asiciali@yahoo.com>
2020-10-24 17:09:55 -0700

    updates

--- src/test/java/com/weborders/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/weborders/step_definitions/LoginStepDefinitions.java
@@ -1,15 +1,18 @@
 package com.weborders.step_definitions;
 
+import com.weborders.pages.HomePage;
 import com.weborders.pages.LoginPage;
 import com.weborders.utilities.ConfigurationReader;
 import com.weborders.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefinitions {
 
     LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
 
     @Given("user is on the landing page")
     public void user_is_on_the_landing_page() {
@@ -20,12 +23,15 @@ public class LoginStepDefinitions {
 
     @When("user logs in")
     public void user_logs_in() {
+        loginPage.login();
 
     }
     @Then("user should see {string} page title")
     public void user_should_see_page_title(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String actualTitle = homePage.getPageTitleText();
+        Assert.assertEquals(string, actualTitle);
+
+
     }
 
 }
