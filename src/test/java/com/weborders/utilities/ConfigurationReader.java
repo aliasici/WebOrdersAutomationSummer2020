dc0e87e7cebc3b9c09283a8d8f2f3818693b3037
aliasici <asiciali@yahoo.com>
2020-12-01 14:48:16 -0800

    I added some note

--- src/test/java/com/weborders/utilities/ConfigurationReader.java
+++ src/test/java/com/weborders/utilities/ConfigurationReader.java
@@ -9,6 +9,9 @@ public class ConfigurationReader {
     private static Properties properties = new Properties();
 
     static {
+        // try with resources
+        //allows to automatically close input stream when object is not used any more
+        // works only for the classes that implement AutoClosable
         try(InputStream in = new FileInputStream("configuration.properties")){
             properties.load(in);
         }catch (Exception e){
