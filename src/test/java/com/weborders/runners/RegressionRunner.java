6981f040c9f7faf09f8b1def4f7d0ef4c95d0d4c
aliasici <asiciali@yahoo.com>
2021-01-30 15:27:28 -0800

    I added regressionrunner class

--- src/test/java/com/weborders/runners/RegressionRunner.java
+++ src/test/java/com/weborders/runners/RegressionRunner.java
@@ -1,2 +1,20 @@
-package com.weborders.runners;public class RegressionRunner {
+package com.weborders.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/weborders/step_definitions",
+        features = "src/test/resources/features",
+        dryRun = false,
+        // monochrome=true,
+        plugin = "json:target/cucumber.json",
+        tags = "@regression"
+        //{"pretty","html:test-output"},json
+
+)
+public class RegressionRunner {
 }
