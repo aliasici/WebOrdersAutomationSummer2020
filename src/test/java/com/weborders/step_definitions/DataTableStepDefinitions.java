15fd9ca2053d43c491fc4183a62f75e718f2f568
aliasici <asiciali@yahoo.com>
2021-02-01 22:41:18 -0800

    Iadded steps for basic data table

--- src/test/java/com/weborders/step_definitions/DataTableStepDefinitions.java
+++ src/test/java/com/weborders/step_definitions/DataTableStepDefinitions.java
@@ -1,2 +1,19 @@
-package com.weborders.step_definitions;public class DataTableStepDefinitions {
+package com.weborders.step_definitions;
+
+import io.cucumber.java.en.Then;
+
+import java.util.List;
+
+public class DataTableStepDefinitions {
+
+    @Then("User should see blow words displayed")
+    public void user_should_see_blow_words_displayed(List<String> fruitList) {
+        // print out size of the list
+        // size method coming from list
+
+        System.out.println("fruitList.size() = " + fruitList.size());
+        System.out.println("fruitList = " + fruitList);
+
+    }
+
 }
