b3982847a54121a9e28490ed29f97bf5afb783d5
aliasici <asiciali@yahoo.com>
2021-02-01 22:52:04 -0800

    I added Etsy search page

--- src/test/java/com/weborders/pages/EtsySearchPage.java
+++ src/test/java/com/weborders/pages/EtsySearchPage.java
@@ -1,2 +1,23 @@
-package com.weborders.pages;public class EtsySearchPage {
+package com.weborders.pages;
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EtsySearchPage {
+
+    // Always here creating first constructor
+
+    public EtsySearchPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(name = "search_query")
+    public WebElement searchBox;
+
+    ////header/div[2]/div[1]/form[1]/div[1]/div[1]/button[1]
+    //(//button[@type='submit'])[1]
+    @FindBy(xpath = "//header/div[2]/div[1]/form[1]/div[1]/div[1]/button[1]")
+    public WebElement serchButton;
 }
