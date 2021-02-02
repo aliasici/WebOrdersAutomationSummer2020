65b7d975b61c1e942c9d3985f967dabbccb4967b
aliasici <asiciali@yahoo.com>
2021-02-02 15:26:58 -0800

    I added some methods

--- src/test/java/com/weborders/pages/WikiSearchPage.java
+++ src/test/java/com/weborders/pages/WikiSearchPage.java
@@ -1,2 +1,25 @@
-package com.weborders.pages;public class WikiSearchPage {
+package com.weborders.pages;
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class WikiSearchPage {
+
+    public WikiSearchPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//input[@id='searchInput']")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//h1[@id='firstHeading']")
+    public WebElement mainHeaderAfterSearch;
+
+    @FindBy(className = "fn")
+    public WebElement imageHeaderAfterSearch;
+
+
+
 }
